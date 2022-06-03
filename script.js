aba557591e28943877c0b2e552b568e6c70c02e0
riz <55377396+Riz-0@users.noreply.github.com>
2022-06-03 20:13:09 +0800

    Add rounds to matches

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 function computerPlay() {
-  // Get the computer to generate rock, paper, or scissors.
+  // Get the computer to generate a move.
   const num = Math.floor(Math.random() * 3) + 1;
   switch(num) {
     case 1:
       return "Rock";
-      break;
     case 2:
       return "Paper";
-      break;
     case 3:
       return "Scissors";
-      break;
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = toTitleCase((playerSelection));
-  console.log(playerSelection);
+  // Checks if player won/lost.
+  if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Scissors" && computerSelection == "Scissors") {
+    return `It's a tie! ${playerSelection} can't beat ${computerSelection}!`;
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  }
+}
+
+function checkValidMove(playerSelection) {
   // Check if user entered a valid move.
   if (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-    console.log("Invalid Move. Please enter Rock/Paper/Scissors.")
-  } else {
-    // Move is valid, check if player won.
-    if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Scissors" && computerSelection == "Scissors") {
-      return `It's a tie! ${playerSelection} can't beat ${computerSelection}!`;
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-      return `You win!  ${playerSelection} beats ${computerSelection}!`;
-    } else {
-      return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    }
+    return false;
+  }
+  return true;
+}
+
+function getMove() {
+  // Get a valid move from user.
+  let playerSelection = prompt("Enter rock/paper/scissors: ");
+  playerSelection = toTitleCase((playerSelection));
+  if (!checkValidMove(playerSelection)) {
+    console.log("Invalid Move. Please enter Rock/Paper/Scissors.");
+    playerSelection = getMove();
+  }
+  return playerSelection
+}
+
+function game() {
+  // Main game loop.
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = getMove();
+    console.log(`You chose ${playerSelection}.`);
+    let computerSelection = computerPlay();
+    console.log(`The computer chose ${computerSelection}.`);
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
   }
 }
 
@@ -38,5 +59,3 @@ function toTitleCase(text) {
   text = text.replace(text.charAt(0), text.charAt(0).toUpperCase());
   return text;
 }
-
-

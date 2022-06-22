012ba617a4c0e5507ebde92998b7297dd8834c46
riz <55377396+Riz-0@users.noreply.github.com>
2022-06-22 19:29:31 +0800

    Add UI

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const player = document.querySelector('#player');
+const comp = document.querySelector('#computer');
+const result = document.querySelector('#result');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (playerScore < 5 && computerScore < 5) {
+      const computerSelection = computerPlay();
+      const playerSelection = button.textContent;
+
+      result.textContent = playRound(playerSelection, computerSelection);
+      player.textContent = `Player (${playerScore}): ${playerSelection}`;
+      comp.textContent = `Computer (${computerScore}): ${computerSelection}`
+
+      checkWinner();
+    }
+  });
+})
+
 function computerPlay() {
   // Get the computer to generate a move.
   const num = Math.floor(Math.random() * 3) + 1;
@@ -20,62 +40,25 @@ function playRound(playerSelection, computerSelection) {
     return `It's a tie! ${playerSelection} can't beat ${computerSelection}!`;
   } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
     playerScore++;
-    return `You win! ${playerSelection} beats ${computerSelection}!`;
+    if (playerScore === 5) {
+      return 'You won! You reached 5 points first';
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
   } else {
     computerScore++;
-    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    if (computerScore === 5) {
+      return 'You lost! Computer reached 5 points first';
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
   }
-}
-
-function checkValidMove(playerSelection) {
-  // Check if user entered a valid move.
-  if (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-    return false;
-  }
-  return true;
-}
-
-function getMove() {
-  // Get a valid move from user.
-  let playerSelection = prompt("Enter rock/paper/scissors: ");
-  playerSelection = toTitleCase((playerSelection));
-  if (!checkValidMove(playerSelection)) {
-    console.log("Invalid Move. Please enter Rock/Paper/Scissors.");
-    playerSelection = getMove();
-  }
-  return playerSelection
 }
 
 function checkWinner() {
-  if (playerScore > computerScore) {
-    console.log(`You win! ${playerScore} : ${computerScore}`);
-  } else if (computerScore > playerScore) {
-    console.log(`Computer wins! ${computerScore} : ${playerScore}`);
-  } else {
-    console.log(`It's a tie! ${computerScore} : ${playerScore}`);
+  if (playerScore === 5) {
+    return 'You Won!';
+  } else if (computerScore === 5) {
+    return "You Lost!";
   }
-}
-
-function game() {
-  playerScore = 0;
-  computerScore = 0;
-
-  // Main game loop.
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = getMove();
-    console.log(`You chose ${playerSelection}.`);
-    let computerSelection = computerPlay();
-    console.log(`The computer chose ${computerSelection}.`);
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-  }
-
-  checkWinner()
-}
-
-function toTitleCase(text) {
-  // Just realised I could've made the computer generate lower case moves instead. ._.
-  text = text.toLowerCase();
-  text = text.replace(text.charAt(0), text.charAt(0).toUpperCase());
-  return text;
 }
